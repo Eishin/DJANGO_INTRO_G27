7396c7dda0113efc037780c23f5993fa9ba1aa18
Eishin <erickjcha@gmail.com>
2026-01-08 23:05:44 -0500

    rty

--- almacen/urls.py
+++ almacen/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index, get_date, get_json, get_html
 
 urlpatterns = [
-    path('/', index, name='index'),
+    path('', index, name='index'),
     path('date/', get_date, name='get_date'),
     path('json/', get_json, name='get_json'),
     path('productos/', get_html, name='get_html'),
