35dbfb3f5cc4d2ecfc350fad4442965c59167026
Eishin <erickjcha@gmail.com>
2026-01-08 22:09:56 -0500

    commit3 django

--- almacen/urls.py
+++ almacen/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, get_date, get_json, get_html
 
 urlpatterns = [
     path('', index, name='index'),
-    path('get_date/', index, name='get_date'),
+    path('date/', get_date, name='get_date'),
+    path('json/', get_json, name='get_json'),
+    path('html/', get_html, name='get_html'),
 ]
--- almacen/views.py
+++ almacen/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from datetime import datetime
 
 # Create your views here.
@@ -10,3 +10,15 @@ def get_date(request):
     date=datetime.now()
     html = f'<h1>fecha actual: {date}</h1>'
     return HttpResponse(html)
+
+def get_json(request):
+    data = {
+        'id': 1,
+        'nombre': 'erik',
+        'mail': 'erik@almacen.com',
+        'age': 25
+    }
+    return JsonResponse(data)
+
+def get_html(request):
+    return render(request, 'index.html')
